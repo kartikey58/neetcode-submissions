e9d3d118fa3346abd8f4f67015ae94c24537a56a
Kartikey Krishna <kartikey.23b1531101@abes.ac.in>
2026-07-04 01:45:10 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+      int currsum= nums[0];
+      int ans = nums[0];
+      for (int i = 1 ; i<nums.length;i++){
+        
+            currsum=Math.max(nums[i],currsum+nums[i]);
+           ans= Math.max(currsum,ans);
+      } 
+      return ans;
+    }
+}
