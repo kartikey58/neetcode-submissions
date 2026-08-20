6437213c477c0d17c48e150908b9d1162919c1e9
Kartikey Krishna <kartikey.23b1531101@abes.ac.in>
2026-08-20 17:21:01 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left =0;
+        int right = height.length-1;
+        int maxarea=0;
+        while(left<right){
+            int width = right-left;
+            int minh = Math.min(height[left], height[right]);
+            int area = minh*width;
+            maxarea = Math.max(maxarea,area);
+            if(height[left]<height[right]){
+            left++;
+            }
+            else{
+            right--;
+            }
+        }
+        return maxarea;
+    }
+}
